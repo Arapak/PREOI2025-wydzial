75275f3730d7bae59cb5eca0c659dc9d05595897
Kajetan Ramsza <kajetan.ramsza@protonmail.com>
2025-01-29 21:48:58 +0100

    initial commit

---
+++ wyd.cpp
@@ -0,0 +1,11 @@
+#include "wydlib.h"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+std::vector<std::vector<int>> Bitek(int n, std::array<int, K> A, std::array<int, K> B) {
+    return vector<vector<int>>(n, vector<int>(n, 1));
+}
+
+std::array<int, K> Bajtazar(std::array<int, 9> A) { return array<int, K>{0}; }
